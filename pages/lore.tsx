14e0626c1810d933710ac6b72f744b6fd833ecad
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-08-06 18:06:42 -0700

    Add PageHero to landing page and lore

--- pages/lore.tsx
+++ pages/lore.tsx
@@ -1,7 +1,12 @@
 import { Flex } from '@chakra-ui/react'
+import { PageHero } from "../components"
 
 const Lore = () => {
-  return <Flex>Lore page</Flex>
+  return (
+    <Flex>
+      <PageHero>Lore</PageHero>
+    </Flex>
+  )
 }
 
 export default Lore
