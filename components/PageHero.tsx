904872069c6c2ea6fcb9194acdec3514a5633a7c
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-08-06 19:32:53 -0700

    Use Section component in PageHero

--- components/PageHero.tsx
+++ components/PageHero.tsx
@@ -1,38 +1,31 @@
-import { Flex, BoxProps, Text, HStack } from '@chakra-ui/react'
-
+import { BoxProps, Text, HStack } from '@chakra-ui/react'
+import { Section } from './'
 export interface IProps extends BoxProps {
   children: React.ReactNode
 }
-export const PageHero: React.FC<BoxProps> = (props) => {
-  const { children } = props
-  return (
-    <HStack flex="1">
-      <Flex
-        {...props}
-        align="flex-start"
-        justify="flex-start"
-        height="calc(35vh + 10vw)"
-        maxH="60vh"
-        backgroundColor='brand.red'
-        backgroundImage='images/metacartel-paris-bw.png'
-        backgroundPosition='center'
-        bgBlendMode='color-dodge'
-        px='min(10vw, 10rem)'
-        py='10vh'
-        flex='1'
+export const PageHero: React.FC<BoxProps> = (props) => (
+  <HStack flex="1">
+    <Section
+      {...props}
+      backgroundImage='images/metacartel-paris-bw.png'
+      bgBlendMode='color-dodge'
+      backgroundColor='brand.red'
+      px='min(10vw, 10rem)'
+      py='10vh'
+      minH="100px"
+      maxH="60vh"
+      height="calc(25vh + 15vw)"
+      backgroundPosition='center'
+    >
+      <Text
+        as="h1"
+        fontFamily='hero'
+        fontSize={['3xl', '4xl', '5xl', '6xl']}
+        lineHeight='1'
       >
-        <Text
-          as="h1"
-          color='black'
-          fontFamily='hero'
-          fontSize={['3xl', '4xl', '5xl', '6xl']}
-          isolation='isolate'
-          lineHeight='1'
-        >
-          {children}
-        </Text>
-      </Flex>
-      {/* TODO: Add news scroll */}
-    </HStack>
-  )
-}
+        {props.children}
+      </Text>
+    </Section>
+    {/* TODO: Add news scroll */}
+  </HStack>
+)
