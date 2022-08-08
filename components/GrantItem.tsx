0a745b28771a318dfc8349c7b69d033f791218c1
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-08-07 17:24:16 -0700

    layout adjustments to GrantItem

--- components/GrantItem.tsx
+++ components/GrantItem.tsx
@@ -1,34 +1,33 @@
 import { BoxProps, Button, Flex, HStack, Image, Text  } from '@chakra-ui/react'
 
 interface GrantItemProps extends BoxProps {
-  date: string
   title: string
-  desc: string
-  url: string
-  amount: string
+  date?: string
+  url?: string
+  amount?: string
 }
 
 export const GrantItem = ( props: GrantItemProps ) => {
 
-  const { date, title, desc, url, amount } = props
+  const { date, title, url, amount } = props
 
   return (
     <Flex
-    borderTopWidth={0}
-    borderRightWidth={2}
-    borderBottomWidth={2}
-    borderLeftWidth={2}
-    borderColor="brand.black"
-    borderStyle={'solid'} 
-    justifyContent={'space-between'}
-    alignItems={'center'}
-    p={'25'}>    
-      <Text as={'h3'}>{title}</Text>
-      <Text>{desc}</Text>
-      <Text>
-          {date} 
-      </Text>
-      <Text>{amount}</Text> 
+      borderTopWidth={0}
+      borderRightWidth={2}
+      borderBottomWidth={2}
+      borderLeftWidth={2}
+      borderColor="brand.black"
+      borderStyle={'solid'} 
+      justifyContent={'space-between'}
+      alignItems={'center'}
+      direction={{base: 'column', md: 'row'}}
+      p={'25'}
+      gap={24}
+    >    
+      <Text me='auto' as={'h3'}>{title}</Text>
+      <Text>{date && date} </Text>
+      <Text>{amount && amount}</Text>
       <Button
         as='a'
         borderWidth={2}
@@ -43,8 +42,9 @@ export const GrantItem = ( props: GrantItemProps ) => {
         _hover={{bg: 'brand.black', color: 'white'}}
         href={url}
         target={'_blank'}
+        disabled={!url}
       >
-        Website
+        {url ? 'Website' : 'No link'}
       </Button>
     </Flex>
   )
