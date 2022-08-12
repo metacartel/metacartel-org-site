44788167898cdb2457cd3b8370dad43b0c0806cd
jonathanprozzi <jonathanprozzi@gmail.com>
2022-08-12 12:22:34 -0400

    split links in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,25 +1,63 @@
-import { Flex, HStack, Box, Button, SimpleGrid, GridItem, Heading, Text, VStack, Link, propNames } from '@chakra-ui/react'
-import { BrandJsonLd, DatasetJsonLd } from 'next-seo'
+import {
+  Flex,
+  HStack,
+  Box,
+  Button,
+  SimpleGrid,
+  GridItem,
+  Heading,
+  Text,
+  VStack,
+  Link,
+  propNames,
+} from "@chakra-ui/react"
+import { BrandJsonLd, DatasetJsonLd } from "next-seo"
+
+const siteLinks = [
+  { name: "Ecosystem", to: "#" },
+  { name: "Grants", to: "#" },
+  { name: "Lore", to: "#" },
+  { name: "Manifesto", to: "#" },
+]
+
+const socialLinks = [
+  { name: "Twitter", to: "#" },
+  { name: "Discord", to: "#" },
+  { name: "Forum", to: "#" },
+  { name: "DAO", to: "#" },
+]
 
 export const Footer = () => {
   return (
-    <Flex flexDirection={'column'} p={25} w={'100%'} justifyContent={'flex-start'} bg={'black'} color={'brand.red'}>
-      <Flex maxWidth={'420px'} margin={'0 auto'} py={25}>
-        <Text as={'h4'} alignSelf={'center'}>MetaCartel</Text>
+    <Flex
+      flexDirection={"column"}
+      p={25}
+      w={"100%"}
+      justifyContent={"flex-start"}
+      bg={"black"}
+      color={"brand.red"}
+    >
+      <Flex maxWidth={"420px"} margin={"0 auto"} py={25}>
+        <Heading as="h4" alignSelf={"center"} fontSize="2xl">
+          MetaCartel
+        </Heading>
       </Flex>
-      <SimpleGrid columns={[1, null, 2]} spacing={25} alignSelf={'flex-start'} maxWidth={'420px'} margin={'0 auto'}>
+      <SimpleGrid
+        columns={[1, null, 2]}
+        spacing={25}
+        alignSelf={"flex-start"}
+        maxWidth={"420px"}
+        margin={"0 auto"}
+      >
         <VStack>
-          <Link>Home</Link>
-          <Link>Ecosystem</Link>
-          <Link>Grants</Link>
-          <Link>Lore</Link>
-          <Link>Manifest</Link>
+          {siteLinks.map((link) => (
+            <Link href={link.to}>{link.name}</Link>
+          ))}
         </VStack>
         <VStack>
-          <Link>Twitter</Link>
-          <Link>Discord</Link>
-          <Link>Forum</Link>
-          <Link>DAO</Link>
+          {socialLinks.map((link) => (
+            <Link href={link.to}>{link.name}</Link>
+          ))}
         </VStack>
       </SimpleGrid>
     </Flex>
