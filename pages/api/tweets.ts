f8b50a75f131aafc45e63a77e91584e5cca971fe
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-08-13 08:06:05 -0700

    yarn format (prettier linting)

--- pages/api/tweets.ts
+++ pages/api/tweets.ts
@@ -1,32 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async (req, res) => {
-  const { TWITTER_BEARER_TOKEN } = process.env;
-  const headers: HeadersInit = new Headers();
-  headers.append('Authorization', `Bearer ${TWITTER_BEARER_TOKEN}`);
+  const { TWITTER_BEARER_TOKEN } = process.env
+  const headers: HeadersInit = new Headers()
+  headers.append("Authorization", `Bearer ${TWITTER_BEARER_TOKEN}`)
 
-  const requestOptions: RequestInit = { method: 'GET', headers };
-  const metaCartelTwitterAccount = '1022327626428379136';
-  const maxResults = 10;
+  const requestOptions: RequestInit = { method: "GET", headers }
+  const metaCartelTwitterAccount = "1022327626428379136"
+  const maxResults = 10
   return new Promise(async (resolve, reject) => {
     try {
       const response: Response = await fetch(
         `https://api.twitter.com/2/users/${metaCartelTwitterAccount}/tweets?max_results=${maxResults}&tweet.fields=attachments%2Cin_reply_to_user_id&media.fields=preview_image_url%2Curl&expansions=attachments.media_keys%2Cin_reply_to_user_id&user.fields=name`,
         requestOptions
-      );
+      )
       if (response.status < 200 || response.status >= 300) {
-        req.status = response.status;
-        req.body = response.statusText;
-        return;
+        req.status = response.status
+        req.body = response.statusText
+        return
       }
-      const { data } = JSON.parse(await response.text());
-      res.status(200).json(data);
+      const { data } = JSON.parse(await response.text())
+      res.status(200).json(data)
     } catch (err) {
-      console.log(err);
-      res.statusCode = 500;
+      console.log(err)
+      res.statusCode = 500
       res.json({
-        msg: 'Something went wrong with requesting the grants data.',
-      });
+        msg: "Something went wrong with requesting the grants data.",
+      })
     }
-  });
-};
+  })
+}
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,6 @@
 export const SOCIAL_LINKS = {
-    TWITTER: 'https://twitter.com/meta_cartel',
-    DISCORD: 'https://discord.gg/PdcY9Yjf', // need perm link
-    FORUM: 'http://forum.metacartel.org/',
-    DAO: 'https://app.daohaus.club/dao/0x64/0xb152b115c94275b54a3f0b08c1aa1d21f32a659a'
+  TWITTER: "https://twitter.com/meta_cartel",
+  DISCORD: "https://discord.gg/PdcY9Yjf", // need perm link
+  FORUM: "http://forum.metacartel.org/",
+  DAO: "https://app.daohaus.club/dao/0x64/0xb152b115c94275b54a3f0b08c1aa1d21f32a659a",
 }
