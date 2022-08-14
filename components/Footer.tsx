832593917b3f61a4fd94c4316b044b60cfa6fe3d
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-08-13 18:08:01 -0700

    feat: Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,27 +1,77 @@
-import { Flex, HStack, Box, Button, SimpleGrid, GridItem, Heading, Text, VStack, Link, propNames } from '@chakra-ui/react'
-import { BrandJsonLd, DatasetJsonLd } from 'next-seo'
+import { Flex, SimpleGrid, Text, VStack, Link, FlexProps } from '@chakra-ui/react'
+import {
+  ECOSYSTEM_PATH,
+  GRANTS_PATH,
+  LORE_PATH,
+  MANIFESTO_PATH,
+  TWITTER_ACCOUNT_URL,
+  DISCORD_INVITE_URL,
+  FORUM_URL,
+  DAOHAUS_URL,
+} from '../constants'
 
-export const Footer = () => {
-  return (
-    <Flex flexDirection={'column'} py={75} pb={150} w={'100%'} justifyContent={'flex-start'} bg={'black'} color={'brand.red'}>
-      <Flex maxWidth={'420px'} margin={'0 auto'} py={25}>
-        <Text as={'h4'} alignSelf={'center'}>MetaCartel</Text>
-      </Flex>
-      <SimpleGrid columns={[1, null, 2]} spacing={25} alignSelf={'flex-start'} maxWidth={'420px'} margin={'0 auto'}>
-        <VStack>
-          <Link>Home</Link>
-          <Link>Ecosystem</Link>
-          <Link>Grants</Link>
-          <Link>Lore</Link>
-          <Link>Manifest</Link>
-        </VStack>
-        <VStack>
-          <Link>Twitter</Link>
-          <Link>Discord</Link>
-          <Link>Forum</Link>
-          <Link>DAO</Link>
-        </VStack>
-      </SimpleGrid>
+const pageLinks = [
+  {
+    label: 'Ecosystem',
+    href: ECOSYSTEM_PATH
+  },
+  {
+    label: 'Grants',
+    href: GRANTS_PATH
+  },
+  {
+    label: 'Lore',
+    href: LORE_PATH
+  },
+  {
+    label: 'Manifesto',
+    href: MANIFESTO_PATH
+  },
+]
+
+const socialLinks = [
+  {
+    label: 'Twitter',
+    href: TWITTER_ACCOUNT_URL
+  },
+  {
+    label: 'Discord',
+    href: DISCORD_INVITE_URL
+  },
+  {
+    label: 'Forum',
+    href: FORUM_URL
+  },
+  {
+    label: 'DAO',
+    href: DAOHAUS_URL
+  },
+]
+
+export const Footer: React.FC<FlexProps> = () => (
+  <Flex
+    as='footer'
+    flexDirection='column'
+    py={20}
+    pb={36}
+    w='100%'
+    color='brand.red'
+    justifyContent='center'
+  >
+    <Flex py={25} justifyContent={'center'}>
+      <Link href='/#' _hover={{ textDecoration: 'none', transform: 'scale(1.05)' }}>
+        <Text as={'h4'} textAlign='start' fontWeight='bold' fontSize='2xl'>
+          MetaCartel
+        </Text>
+      </Link>
     </Flex>
-  )
-}
+    <SimpleGrid columns={{ base: 1, sm: 2 }} spacing={25} alignSelf={'center'}>
+      <VStack>
+        {pageLinks.map(({ label, href }) => <Link href={href}>{label}</Link>)}
+      </VStack>
+      <VStack>
+        {socialLinks.map(({ label, href }) => <Link href={href}>{label}</Link>)}
+      </VStack>
+    </SimpleGrid>
+  </Flex>
+)
---
+++ constants/index.ts
@@ -0,0 +1,12 @@
+// Internal paths
+export const ECOSYSTEM_PATH = "/ecosystem"
+export const GRANTS_PATH = "/grants"
+export const LORE_PATH = "/lore"
+export const MANIFESTO_PATH = "/manifesto"
+
+// External links
+export const TWITTER_ACCOUNT_URL = "https://twitter.com/meta_cartel"
+export const DISCORD_INVITE_URL = "https://discord.gg/PdcY9Yjf"
+export const FORUM_URL = "http://forum.metacartel.org/"
+export const DAOHAUS_URL =
+  "https://app.daohaus.club/dao/0x64/0xb152b115c94275b54a3f0b08c1aa1d21f32a659a"
