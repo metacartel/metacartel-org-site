1d54edc157fe914181a6dad61082ee31d2e69188
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-08-13 23:30:41 -0700

    update cta scroller with nav indicators

--- components/CtaScrollBar.tsx
+++ components/CtaScrollBar.tsx
@@ -1,6 +1,8 @@
-import { Flex, FlexProps, Button, ButtonProps } from '@chakra-ui/react'
+import { useState } from 'react'
+import { Flex, FlexProps } from '@chakra-ui/react'
 import { CtaItem, Icon, IconName } from './'
 
+// TODO: Replace action item dummy data
 const actionItems = [
   {
     label: 'Taco Tuesday lorem ipsum dolor sit amet',
@@ -39,36 +41,55 @@ const actionItems = [
   },
 ]
 
-interface NavButtonProps extends ButtonProps {
+interface NavIndicatorProps extends FlexProps {
   isLeft?: boolean
+  isScrolledLeft?: boolean
+  isScrolledRight?: boolean
 }
-const NavButton: React.FC<NavButtonProps> = ({isLeft=false, ... props}) => (
-  <Button
+const NavIndicator: React.FC<NavIndicatorProps> = (props) => (
+  <Flex
     bg='brand.offWhite'
     color="brand.red"
     position='absolute'
     borderRadius='none'
-    transform={isLeft && 'scale(-1)'}
-    left={isLeft ? '0' : 'auto'}
-    right={isLeft ? 'auto' : '0'}
     height="100%"
     width='fit-content'
-    px={3}
+    p={3}
+    transition="transform 0.2s ease-in-out"
     {...props}
   >
     <Icon name={"play" as IconName} />
-  </Button>
+  </Flex>
 )
 
 export const CtaScrollBar: React.FC<FlexProps> = (props) => {
+  const [isScrolledLeft, setIsScrolledLeft] = useState(false)
+  const [isScrolledRight, setIsScrolledRight] = useState(false)
+  
+  const handleScroll = ({ target }) => {
+    const { scrollLeft, scrollWidth, clientWidth } = target
+    const TOLERANCE = 16
+    if (scrollLeft < TOLERANCE) {
+      setIsScrolledLeft(true)
+      return
+    } 
+    if (scrollLeft > scrollWidth - clientWidth - TOLERANCE) {
+      setIsScrolledRight(true)
+      return
+    }
+    setIsScrolledLeft(false)
+    setIsScrolledRight(false)
+  }
+  console.log({isScrolledLeft, isScrolledRight})
   return (
-    <Flex {...props} position='relative' borderBottom="4px" borderColor="brand.red" maxW="100vw">
-      {/* TODO: Wire up logic for buttons */}
-      <NavButton isLeft />
-      <NavButton />
-      {actionItems.map(({ label, action, icon, color, url }) => (
-        <CtaItem label={label} action={action} icon={icon} color={color} url={url} />
-      ))}
+    <Flex {...props} position='relative' borderBottom="4px" borderColor="brand.red">
+      <NavIndicator transform={`scale(-1) translateX(${isScrolledRight ? '0' : '100%'})`} left='0' />
+      <NavIndicator transform={`translateX(${isScrolledLeft ? '0' : '100%'})`} right='0' />
+      <Flex maxW="100vw" overflowX='scroll' onScroll={handleScroll}>
+        {actionItems.map(({ label, action, icon, color, url }) => (
+          <CtaItem label={label} action={action} icon={icon} color={color} url={url} />
+        ))}
+      </Flex>
     </Flex>
   )
 }
