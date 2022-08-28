b8d9d274a575c4ea663c2f113707a3e20668cdaa
oovg <office@vengist.com>
2022-08-28 16:28:56 -0700

    adds metaticker

---
+++ components/MetaTicker.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import { Flex, FlexProps } from '@chakra-ui/react'
+import { HorizontalTicker } from 'react-infinite-ticker'
+import { CtaItem, Icon, IconName } from './'
+
+const actionItems = [
+  {
+    label: 'MCON2: Get your spicy DAO fix',
+    icon: 'mountains',
+    color: 'brand.sun',
+    url: 'https://mcon.fun',
+  },
+  {
+    label: 'Hydra is Rising',
+    action: 'View Whitepaper',
+    icon: 'sword',
+    color: 'brand.purp',
+    url: 'https://github.com/metacartel/hydra-DAO/blob/main/Whitepaper.pdf',
+  },
+  {
+    label: 'Taco Townhall every Tuesday (6pm CEST)',
+    action: 'Members Only',
+    icon: 'key',
+    color: 'brand.taco',
+    url: 'https://discord.gg/3CUVkcaF',
+  },
+  {
+    label: 'Public Nouns Launching Soon™️',
+    action: 'Check for Auction',
+    icon: 'shopping-cart',
+    color: 'brand.green',
+    url: 'https://publicnouns.wtf',
+  },
+]
+
+export const MetaTicker: React.FC = (props) => {
+  return (
+    <Flex {...props} position='relative' borderBottom="4px" borderColor="brand.red">
+      <Flex maxW="100vw" overflowX='scroll'>
+        <HorizontalTicker duration={25000}>
+        {actionItems.map(({ label, action, icon, color, url }) => (
+          <CtaItem label={label} action={action} icon={icon} color={color} url={url} key={label} />
+        ))}
+        </HorizontalTicker>
+      </Flex>
+    </Flex>
+  )
+}
