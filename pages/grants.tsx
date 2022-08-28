513ecea81b54dcfca5d67ecf0a5fb8ca61d144c2
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-08-28 09:36:11 -0700

    fix: mobile responsiveness

--- pages/grants.tsx
+++ pages/grants.tsx
@@ -10,13 +10,16 @@ const Grants: FC = () => {
     <Flex flexDirection='column' w='100%'>
       <PageHero bg="brand.red">Grants</PageHero>
       <Section py={75}>
-        <Input
-          placeholder="Search grant recipients"
-          value={filter}
-          onChange={handleInputChange}
-          mb={8}
-          color="bg"
-        />
+        <Flex mx={{base: 6, xl: 0}}>
+          <Input
+            placeholder="Search grant recipients"
+            value={filter}
+            onChange={handleInputChange}
+            mb={8}
+            color="bg"
+            borderRadius='none'
+          />
+        </Flex>
         <GrantList filter={filter} />
       </Section>
     </Flex>
