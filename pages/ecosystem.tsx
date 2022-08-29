aa15d322421c2a49fd363eac3b5fe73a9d53b7c6
oovg <office@vengist.com>
2022-08-28 17:31:37 -0700

    card layout for ecosystem page

--- pages/ecosystem.tsx
+++ pages/ecosystem.tsx
@@ -1,12 +1,12 @@
 import { Flex } from '@chakra-ui/react'
-import { EcosystemList, PageHero, Section } from '../components'
+import { EcosystemTable, PageHero, Section } from '../components'
 
 const Ecosystem = () => {
   return (
   <Flex flexDirection='column' w='100%'>
     <PageHero bg="brand.red">Ecosystem</PageHero>
     <Section py={75}>
-      <EcosystemList />
+      <EcosystemTable />
     </Section>
   </Flex>
   );
