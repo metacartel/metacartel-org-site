cf0228e0ce043171381991a377e5a27f1c75b4ae
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-08-28 22:41:51 -0700

    linting

--- components/EcosystemGrid.tsx
+++ components/EcosystemGrid.tsx
@@ -12,6 +12,7 @@ export const EcosystemGrid: React.FC<EcosystemListProps> = ({ color = 'brand.sun
   const [data, setData] = useState([])
   useEffect(() => {
     ;(async () => {
+      // TODO: Global context state management for fetched data
       const data: Array<{[key: string]: any}> = await getData('./api/get_grants')
       const filteredData = data.filter(({fields}) => fields['isEcosystem'])
       const maxHomepageItems = 10
@@ -35,55 +36,52 @@ export const EcosystemGrid: React.FC<EcosystemListProps> = ({ color = 'brand.sun
       <SimpleGrid columns={[1, null, 2, 3]} spacing='40px'>
         {data.map(({ desc, websiteUrl, logoUrl, projectName }) => (
           <Flex
-          flexDirection="column" 
-          minHeight={'200px'} 
-          color="white"
-          border='2px'
-          borderColor={color}
-          p={5}
-          _hover={{transform: 'scale(1.02)', transition: 'transform 0.2s ease-in-out'}}
-          transform="scale(1)"
-          transition='transform 0.2s ease-in-out'
-        >    
-        <Flex alignItems="center" mb={8}>
-            <Link
-            href={websiteUrl}
-            borderRadius='full'
-            _hover={{
-              outline: '2px solid',
-              outlineColor: color
-            }}
-            aria-label={projectName}
-            bgImage={`url(${logoUrl})`}
-            h="60px"
-            w="60px"
-            bgSize="contain"
-            />
-            <Text fontSize={['xl', null, 'xl']} ml="auto">{projectName}</Text>
-
-          
-        </Flex>
+            flexDirection="column" 
+            minHeight={'200px'} 
+            color="white"
+            border='2px'
+            borderColor={color}
+            p={5}
+            _hover={{transform: 'scale(1.02)', transition: 'transform 0.2s ease-in-out'}}
+            transform="scale(1)"
+            transition='transform 0.2s ease-in-out'
+          >    
+            <Flex alignItems="center" mb={8}>
+              <Link
+                href={websiteUrl}
+                borderRadius='full'
+                _hover={{
+                  outline: '2px solid',
+                  outlineColor: color
+                }}
+                aria-label={projectName}
+                bgImage={`url(${logoUrl})`}
+                h="60px"
+                w="60px"
+                bgSize="contain"
+              />
+              <Text fontSize={['xl', null, 'xl']} ml="auto">{projectName}</Text>
+            </Flex>
             <Text mb={8}>{desc}</Text>
             <Flex justify="end" mt="auto">
-            { websiteUrl ? (
-              <IconButtonLink
-                icon='link'
-                title='Link'
-                href={websiteUrl}
-                color={color}
-                isExternal
-                justify='end'
-                size="xs"
-              />
-            ) : (
-              <IconButton
-                icon="link"
-                title='No link'
-                color={color}
-                disabled
-                size="sm"
-              />
-            )}
+              {websiteUrl ? (
+                <IconButtonLink
+                  icon='link'
+                  title='Link'
+                  href={websiteUrl}
+                  color={color}
+                  isExternal
+                  size="xs"
+                />
+              ) : (
+                <IconButton
+                  icon="link"
+                  title='No link'
+                  color={color}
+                  disabled
+                  size="sm"
+                />
+              )}
             </Flex>
           </Flex>
         ))}
