7cb3a2f68807c32020e975697952c10d52480984
jonathanprozzi <jonathanprozzi@gmail.com>
2022-08-31 14:15:31 -0400

    added session support on index -- added types for auth and NextPage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import type { GetServerSideProps, NextPage } from "next"
+import { unstable_getServerSession } from "next-auth"
+import { getAuthOptions } from "./api/auth/[...nextauth]"
+import { Box, Flex, Text } from "@chakra-ui/react"
 import {
   CalendarList,
   EcosystemList,
@@ -14,17 +17,32 @@ import {
   WipList,
 } from "../components"
 
-const Home = () => {
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  return {
+    props: {
+      session: await unstable_getServerSession(req, res, getAuthOptions(req)),
+    },
+  }
+}
+
+const Home: NextPage = () => {
   return (
-    <Flex direction={'column'}>
+    <Flex direction={"column"}>
       <PageHero>
-        <Text whiteSpace='nowrap' fontFamily='inherit' textAlign='center'>
-          A supportive ecosystem of<br/>creators and builders at the<br />forefront of web3 and DAOs
+        <Text whiteSpace="nowrap" fontFamily="inherit" textAlign="center">
+          A supportive ecosystem of
+          <br />
+          creators and builders at the
+          <br />
+          forefront of web3 and DAOs
         </Text>
       </PageHero>
       <MottoSection />
       <Section>
-        <Box display={{base: 'inline', md: 'flex'}} borderY="0.5rem solid black">
+        <Box
+          display={{ base: "inline", md: "flex" }}
+          borderY="0.5rem solid black"
+        >
           <ShotsFired />
           <PhotoCarousel />
         </Box>
@@ -32,60 +50,61 @@ const Home = () => {
       <TwitterSection />
       <Flex direction="column" gap={[8, null, 16]} mt={[8, null, 16]}>
         <SectionList
-          justifyContent={'space-between'}
-          alignItems={'center'}
+          justifyContent={"space-between"}
+          alignItems={"center"}
           icon="calendar"
           title="Upcoming Events"
           color="brand.purp"
           p={25}
           maxW="container"
-          mx='auto'
+          mx="auto"
           pageUrl="/events"
         >
-          <CalendarList color="brand.purp"/>
+          <CalendarList color="brand.purp" />
         </SectionList>
         <SectionList
-          justifyContent={'space-between'}
+          justifyContent={"space-between"}
           icon="mountains"
           title="Ecosystem"
-          alignItems={'center'}
+          alignItems={"center"}
           p={25}
-          mx='auto'
+          mx="auto"
           color="brand.sun"
-          pageUrl="/ecosystem">
+          pageUrl="/ecosystem"
+        >
           <EcosystemList color="brand.sun" />
         </SectionList>
         <SectionList
-          justifyContent={'space-between'}
-          alignItems={'center'}
+          justifyContent={"space-between"}
+          alignItems={"center"}
           icon="bag"
           title="Recent Grants"
           color="brand.green"
           p={25}
           maxW="container"
-          mx='auto'
+          mx="auto"
           pageUrl="/grants"
         >
           <GrantList color="brand.green" />
         </SectionList>
         <SectionList
-          justifyContent={'space-between'}
-          alignItems={'center'}
+          justifyContent={"space-between"}
+          alignItems={"center"}
           icon="test-tube"
           title="Works in Progress"
           color="brand.teal"
           p={25}
           maxW="container"
-          mx='auto'
+          mx="auto"
           pageUrl="https://app.dework.xyz/metacartel"
           linkLabel="View Dework board"
         >
-          <WipList color='brand.teal' />
+          <WipList color="brand.teal" />
         </SectionList>
         <Membership />
       </Flex>
     </Flex>
-  );
+  )
 }
 
 export default Home
