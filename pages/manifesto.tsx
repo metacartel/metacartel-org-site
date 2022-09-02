52d46ab82968e31bdba0403e6bade496b2cddc0b
jonathanprozzi <jonathanprozzi@gmail.com>
2022-09-02 12:44:52 -0400

    added react-markdown to render

--- pages/manifesto.tsx
+++ pages/manifesto.tsx
@@ -1,17 +1,36 @@
-import { Flex } from "@chakra-ui/react"
-import { PageHero } from "../components"
+import { useEffect, useState } from "react"
+import ReactMarkdown from "react-markdown"
 import { getManifesto } from "../libs/arweave"
+import { Box, Flex } from "@chakra-ui/react"
+import { PageHero, Section } from "../components"
 
-const fetchManifesto = async () => {
-  const manifesto = await getManifesto()
-  console.log("manifesto", manifesto)
-}
-
-fetchManifesto()
 const Manifesto = () => {
+  const [manifesto, setManifesto] = useState({})
+
+  useEffect(() => {
+    const fetchManifesto = async () => {
+      const manifestoResponse = await getManifesto()
+      setManifesto(manifestoResponse.data["manifesto"])
+    }
+    fetchManifesto()
+  }, [])
+
+  useEffect(() => {
+    console.log("manifesto", manifesto)
+  }, [manifesto])
+
   return (
     <Flex>
-      <PageHero bg="brand.red">Manifesto</PageHero>
+      {/* <PageHero bg="brand.red">Manifesto</PageHero> */}
+      <Section py={75}>
+        {manifesto ? (
+          <Flex mx={{ base: 6, xl: 0 }} direction="column" alignItems="center">
+            <ReactMarkdown children={manifesto.toString()} />
+          </Flex>
+        ) : (
+          <Flex>Loading...</Flex>
+        )}
+      </Section>
     </Flex>
   )
 }
