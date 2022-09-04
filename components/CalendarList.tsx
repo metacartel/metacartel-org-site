7361f8be6417c52a1d38d1d9c425cc1d60e1ec8b
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-09-04 11:39:10 -0700

    fix: date rendering logic

--- components/CalendarList.tsx
+++ components/CalendarList.tsx
@@ -45,15 +45,38 @@ export const CalendarList: React.FC<CalendarListProps> = ({ color }) => {
       const MAX_ITEMS = pathname === "/" ? 6 : 100
       try {
         const data: EventItem[] = await getData("api/get_events")
-        const mappedData: CalendarListData[] = data.map(({ id, htmlLink, summary, description, start, end, etag }) => ({
-          id, title: summary, desc: description,
-          etag: etag.replaceAll("\"", ""),
-          start: start.dateTime ? format(new Date(start.dateTime), "P") : format(new Date(start.date), "P"),
-          end: end.dateTime ? format(new Date(end.dateTime), "P") : format(new Date(end.date), "P"),
-          timezone: start.timeZone,
-          url: htmlLink
-        }))
-        // Remove items with duplicate etags, keeping only the first one
+        const mappedData: CalendarListData[] = data.map(({ id, htmlLink, summary, description, start, end, etag }) => {
+          const dateRange = { start: '', end: '' }
+          const isAllDay = !start.timeZone
+          // If all-day event (no timezone indicated), move "end" date back by one day (as it lands on 00:00:00 the next day)
+          if (isAllDay) {
+            // Parse string to create Date object in users locale for all-day events
+            // Prevents date from shifting depending on users timezone
+            const splitStartDate = start.date.split("-")
+            const splitEndDate = end.date.split("-")
+            const splitStartDateNumbers = splitStartDate.map((s) => parseInt(s))
+            const splitEndDateNumbers = splitEndDate.map((s) => parseInt(s))
+            // [0] Year, [1] Month, [2] Day
+            const startDateInitString = `${splitStartDateNumbers[1]}/${splitStartDateNumbers[2]}/${splitStartDateNumbers[0]}`
+            const endDateInitString = `${splitEndDateNumbers[1]}/${splitEndDateNumbers[2] - 1}/${splitEndDateNumbers[0]}`
+            // Format all-day events with only date, "P"
+            dateRange.start = format(new Date(startDateInitString), "P")
+            dateRange.end = format(new Date(endDateInitString), "P")
+          } else {
+            // Format in-day events with date plus time range, "Pp" > "p"
+            dateRange.start = format(new Date(start.dateTime), "Pp")
+            dateRange.end = format(new Date(end.dateTime), "p")
+          }
+          return {
+            id, title: summary, desc: description,
+            etag: etag.replaceAll("\"", ""),
+            timezone: start.timeZone,
+            start: dateRange.start,
+            end: dateRange.end,
+            url: htmlLink
+          }
+        })
+        // Remove items with duplicate etags (recurring events), keeping only the first one
         let etagSet: string[] = []
         const filteredData = mappedData.filter(({ etag }) => {
           if (etagSet.includes(etag)) return false
