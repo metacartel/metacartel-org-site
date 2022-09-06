737dd066426c92a9d1f265ef3c4e32a49798d17e
jonathanprozzi <jonathanprozzi@gmail.com>
2022-09-06 11:54:40 -0600

    moved table name to airtable.ts

--- libs/airtable.ts
+++ libs/airtable.ts
@@ -1,12 +1,12 @@
 import Airtable from "airtable"
 
-const { AIR_API_KEY, BASE_ID, GRANTS_TABLE_NAME, EVENTS_TABLE_NAME } =
+const { AIR_API_KEY, BASE_ID, GRANTS_TABLE_NAME, EVENTS_TABLE_NAME, MANIFESTO_TABLE_NAME } =
   process.env
 const base = new Airtable({ apiKey: AIR_API_KEY }).base(BASE_ID)
 
 const eventsTable = base(EVENTS_TABLE_NAME)
 const grantsTable = base(GRANTS_TABLE_NAME)
-const manifestoSignaturesTable = base("Manifesto Signatures")
+const manifestoSignaturesTable = base(MANIFESTO_TABLE_NAME)
 
 const getMinifiedRecord = ({ id, fields }) => ({ id, fields })
 
