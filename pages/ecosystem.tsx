55b8e197c474f247eb442ab0a7d49151ff430149
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-03-26 09:42:43 +0100

    add PageMetadata to page components

--- pages/ecosystem.tsx
+++ pages/ecosystem.tsx
@@ -1,9 +1,10 @@
 import { Flex } from "@chakra-ui/react"
-import { EcosystemGrid, PageHero, Section } from "../components"
+import { EcosystemGrid, PageHero, PageMetadata, Section } from "../components"
 
 const Ecosystem = () => {
   return (
   <Flex flexDirection="column" w="100%">
+    <PageMetadata title="Ecosystem" description="The MetaCartel ecosystem" />
     <PageHero>Ecosystem</PageHero>
     <Section py={75}>
       <EcosystemGrid />
